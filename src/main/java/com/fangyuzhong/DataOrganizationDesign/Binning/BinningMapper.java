4797d4fcfa97671d953d8985758451ce1865880e
fangyuzhong2016 <906328924@qq.com>
2017-07-13 10:28:02 +0800

    增加分箱模式算法代码

---
+++ src/main/java/com/fangyuzhong/DataOrganizationDesign/Binning/BinningMapper.java
@@ -0,0 +1,73 @@
+package com.fangyuzhong.DataOrganizationDesign.Binning;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordWriter;
+
+import org.apache.hadoop.mapred.lib.MultipleOutputFormat;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.util.Progressable;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * Created by fangyuzhong on 17-7-13.
+ */
+public class BinningMapper extends Mapper<Object, Text, Text, NullWritable>
+{
+
+    private MultipleOutputs<Text,NullWritable> mos = null;
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    protected void setup(Context context)
+    {
+        mos = new MultipleOutputs(context);
+    }
+
+    /**
+     * map函数
+     *
+     * @param key
+     * @param value
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException
+    {
+        String[] valueSplits = value.toString().split(",");
+        if(valueSplits.length>=7)
+        {
+            //按照性别输出
+            String strMF = valueSplits[2];
+            if(strMF!=null&&strMF!="")
+            {
+                if("M".equals(strMF))
+                {
+                    mos.write("bins",value,NullWritable.get(),"男-tag");
+                }
+                else if("F".equals(strMF))
+                {
+                    mos.write("bins",value,NullWritable.get(),"女-tag");
+                }
+                else
+                {
+                    mos.write("bins",value,NullWritable.get(),"其他-tag");
+                }
+            }
+        }
+    }
+
+    @Override
+    protected  void cleanup(Context context) throws IOException,InterruptedException
+    {
+        mos.close();
+    }
+}
